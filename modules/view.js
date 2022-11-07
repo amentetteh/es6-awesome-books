e424b84a7d6b5ecf0f6d5e8c2062529846cbf30b
Amen TETTEH <tkamen@outlook.com>
2022-11-07 20:21:44 +0000

    Update view.js to fix linter errors

--- modules/view.js
+++ modules/view.js
@@ -1,7 +1,6 @@
-import Book from "./model/book.js";
-import Contact from "./model/contact.js";
-import { DateTime } from "../luxon.js";
-
+import Book from './model/book.js';
+import Contact from './model/contact.js';
+import { DateTime } from '../luxon.js';
 
 class View {
   static createTag = (tagName, textContent = null, className = null) => {
@@ -12,12 +11,12 @@ class View {
   };
 
   static createBookRow = (book) => {
-    const bookRow = View.createTag("tr", null, "rowTR");
-    const titleTD = View.createTag("td", null, "titleTD");
-    const authorTD = View.createTag("td", null, "authorTD");
-    const idTD = View.createTag("td", null, "idTD hidden");
-    const buttonTD = View.createTag("td", null, "buttonTD buttonRemove");
-    const buttonRemove = View.createTag("button", "Remove", "buttonRemove");
+    const bookRow = View.createTag('tr', null, 'rowTR');
+    const titleTD = View.createTag('td', null, 'titleTD');
+    const authorTD = View.createTag('td', null, 'authorTD');
+    const idTD = View.createTag('td', null, 'idTD hidden');
+    const buttonTD = View.createTag('td', null, 'buttonTD buttonRemove');
+    const buttonRemove = View.createTag('button', 'Remove', 'buttonRemove');
     buttonTD.appendChild(buttonRemove);
 
     const rowItems = [idTD, titleTD, authorTD, buttonTD];
@@ -30,9 +29,9 @@ class View {
     titleTD.textContent = `“${book.title}”`;
     authorTD.textContent = book.author;
 
-    buttonRemove.addEventListener("click", (event) => {
+    buttonRemove.addEventListener('click', (event) => {
       const { target } = event;
-      const bookNode = target.parentNode.parentNode.getElementsByTagName("td");
+      const bookNode = target.parentNode.parentNode.getElementsByTagName('td');
       const ID = bookNode[0].innerHTML;
       const Title = bookNode[1].innerHTML;
       const Author = bookNode[2].innerHTML;
@@ -62,20 +61,20 @@ class View {
   };
 
   static displaySection = (section, sectionArray) => {
-    if (!section.classList.contains("show")) {
-      section.classList.toggle("show");
+    if (!section.classList.contains('show')) {
+      section.classList.toggle('show');
     }
     for (let i = 0; i < sectionArray.length; i += 1) {
       if (sectionArray[i] !== section) {
-        if (!sectionArray[i].classList.contains("hidden")) {
-          sectionArray[i].classList.toggle("hidden");
+        if (!sectionArray[i].classList.contains('hidden')) {
+          sectionArray[i].classList.toggle('hidden');
         }
       }
     }
   };
 
   static removeBookFromUI = (item) => {
-    if (item.classList.contains("buttonRemove")) {
+    if (item.classList.contains('buttonRemove')) {
       item.parentElement.parentElement.remove();
     }
   };
@@ -85,30 +84,30 @@ class View {
   };
 
   static clearForm = () => {
-    document.querySelector("#bookID").value = "";
-    document.querySelector("#booktitle").value = "";
-    document.querySelector("#bookauthor").value = "";
+    document.querySelector('#bookID').value = '';
+    document.querySelector('#booktitle').value = '';
+    document.querySelector('#bookauthor').value = '';
   };
 
   static displayContact = () => {
-    const contactUl = View.createTag("ul", null, "contact-ul");
+    const contactUl = View.createTag('ul', null, 'contact-ul');
     const contact = new Contact();
     const contactlist = Object.values(contact);
     for (let i = 0; i < contactlist.length; i += 1) {
-      const contactLi = View.createTag("li", null, "contact-li");
+      const contactLi = View.createTag('li', null, 'contact-li');
       contactLi.textContent = contactlist[i];
       contactUl.appendChild(contactLi);
     }
-    const contactList = document.querySelector(".contact-list");
+    const contactList = document.querySelector('.contact-list');
     contactList.appendChild(contactUl);
   };
 
   static displayDate = () => {
-    const datePlaceholder = document.querySelector("#datetime");
+    const datePlaceholder = document.querySelector('#datetime');
 
     // eslint-disable-next-line no-undef
     datePlaceholder.textContent = DateTime.local().toLocaleString(
-      DateTime.DATETIME_FULL
+      DateTime.DATETIME_FULL,
     );
   };
 }
